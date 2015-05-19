21b4844d2b751a8916d788c3721c374aa703dcc3
carl.xu <nicxzq@126.com>
2015-05-19 11:12:50 +0800

    add hello.py

---
+++ hello.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#print 'hello world'
+#name = raw_input('please input ur name: ')
+#print 'hello', name
+#a=raw_input('please enter a number')
+#a=input('please enter a number: ')
+
+#if int(a)>=100:
+#    print a
+#else:
+#    print -a
+
+#print '...'
+# print '\xe4\xb8\xad\xe6\x96\x87'.encode('utf-8')
+# print '\xe4\xb8\xad\xe6\x96\x87'
+
+#if else
+# age=input('please enter ur age: ')
+# if age >=6:
+#     print 'teenager'
+# elif age >= 18:
+#     print 'adult'
+# else:
+#     print 'kid'
+
+#for
+#sum = 0
+# for x in xrange(1,100):
+#     sum += x
+# print sum
+
+# while sum==0:
+#     print sum
+
+#list[]  tuple() 
+
+d = {'xzq':100, 'xhy':101, 'xtx':True}
+# if 'xtx' in d:
+#     d['xtx']= 102
+# for x in d:
+#     print x,d[x]
+# d.pop(x)
+# print d
+
+#set
+# s=set([1,2,True])
+# print s
+# t=(1,2,3,3)
+# s.add(t)
+# print s
+# l = [1,2,3,3]
+# print set(l)
+
+def my_abs(x):
+    if x>=0:
+        return x
+    else:
+        return -x
